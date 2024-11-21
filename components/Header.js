6bd933751787bc15e6639a4c74a7c2abd4727955
Nathan Pare <nathanpare@Nathans-MBP.lan>
2024-11-21 11:30:26 -0800

    file structure

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
     <div className='sticky top-0 h-24 flex justify-between
      items-center px-5'>
       <Image onClick={() => router.push("/")} className='cursor-pointer mt-6'
-        src="/Mainland media edited.png"
+        src={"/static/images/Mainland media edited.png"}
         alt='' height={100} width={100} />
       <Dropdown />
     </div>
